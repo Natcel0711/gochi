c2a8e847dcf9cb928dc47e61b7be728770c3d093
Natcel0711 <natcelnieves@gmail.com>
2023-01-14 23:08:52 -0400

    added update

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -70,6 +70,27 @@ func GetUserByID(env *models.Env) http.HandlerFunc {
 	}
 }
 
+func GetUserByEmail(env *models.Env) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		email := chi.URLParam(r, "email")
+		row := env.Db.QueryRow("SELECT id, name, email, password, created_at, updated_at FROM public.users WHERE email=$1", email)
+		var user models.User
+		switch err := row.Scan(&user.Id, &user.Name, &user.Email, &user.Password, &user.Created_at, &user.Updated_at); err {
+		case sql.ErrNoRows:
+			w.Write([]byte("No rows were returned"))
+		case nil:
+			j, err := json.Marshal(user)
+			if err != nil {
+				w.Write([]byte("error turning to json"))
+				return
+			}
+			w.Write([]byte(j))
+		default:
+			w.Write([]byte(fmt.Sprintf("Error while mapping user: %v", err)))
+		}
+	}
+}
+
 func CreateUser(env *models.Env) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var user models.CreatedUser
@@ -78,7 +99,6 @@ func CreateUser(env *models.Env) http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		log.Println(user)
 		var id int
 		err = env.Db.QueryRow(`INSERT INTO public.users (name, email, password, created_at, updated_at) VALUES($1,$2,$3,$4,$5) RETURNING id`, user.Name, user.Email, user.Password, time.Now(), time.Now()).Scan(&id)
 		if err != nil {
@@ -86,5 +106,30 @@ func CreateUser(env *models.Env) http.HandlerFunc {
 			return
 		}
 		w.Write([]byte(fmt.Sprintf("{\"ID\":%d, \"Success\": %t}", id, true)))
+	}
+}
+
+func UpdateUser(env *models.Env) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user models.CreatedUser
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		row := env.Db.QueryRow("SELECT name, email, password FROM public.users WHERE email=$1", user.Email)
+		var foundUser models.CreatedUser
+		switch err := row.Scan(&foundUser.Name, &foundUser.Email, &foundUser.Password); err {
+		case sql.ErrNoRows:
+			w.Write([]byte("No user exists"))
+		case nil:
+			row := env.Db.QueryRow("UPDATE public.users SET name = $1, password = $2 WHERE email = $3", user.Name, user.Password, user.Email)
+			if row.Err() != nil {
+				w.Write([]byte(fmt.Sprintf("Error updating user: %v", err)))
+			}
+			w.Write([]byte(fmt.Sprintf("{\"Success\": %t}", true)))
+		default:
+			w.Write([]byte(fmt.Sprintf("Error while mapping user: %v", err)))
+		}
 	}
 }
